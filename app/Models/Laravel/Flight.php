b729b8cbcd22904fb2c79db103891bdc65ec66c0
startnay <start.nay@gmail.com>
2017-03-23 06:23:51 +0700

    create Flight Model and Migration of it

---
+++ app/Models/Laravel/Flight.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Laravel;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Flight extends Model
+{
+   public $incrementing = false;
+   public $timestamps=false;
+}
